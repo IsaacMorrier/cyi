89b4d7246d913e56b300b96478917b92305205fe
Isaac Morrier <isaacmorrier@gmail.com>
2020-09-14 21:02:03 -0400

    connect cms and sapper

--- src/routes/work/[slug].svelte
+++ src/routes/work/[slug].svelte
@@ -2,11 +2,10 @@
 	export async function preload({ params, query }) {
 		// the `slug` parameter is available because
 		// this file is called [slug].svelte
-		const res = await this.fetch(`blog/${params.slug}.json`);
-		const data = await res.json();
+		const res = await this.fetch(`_work/${params.slug}.json`);
 
 		if (res.status === 200) {
-			return { post: data };
+			return { postMd: await res.text() };
 		} else {
 			this.error(res.status, data.message);
 		}
@@ -14,7 +13,19 @@
 </script>
 
 <script>
-	export let post;
+	import fm from 'front-matter';
+  import MarkdownIt from 'markdown-it';
+  
+  export let postMd;
+  
+  const md = new MarkdownIt();
+  
+  $:frontmatter = fm(postMD);
+  $: post = {
+    ...frontMatter.attributes,
+    html: md.render(frontMatter.body)
+  };
+  
 </script>
 
 <style>
