2ca4c30cfa9c14f281b6334e31a5da64f8d17c7c
Isaac Morrier <isaacmorrier@gmail.com>
2020-11-16 22:49:43 -0500

    add scroll movement to portals

--- src/components/Portal.svelte
+++ src/components/Portal.svelte
@@ -5,14 +5,18 @@
     export let scale: number;
     export let containerHeight: number;
     export let containerWidth: number;
-    // export let y;
+    export let scrollY: number;
 
-    let diameter: string, x: string, y: string, z: string;
+    let diameter: string, x: string, y: string, z: string, i: number, j: number, k: number, t: string;
     $: {
-        diameter = containerWidth + 'px';
+        diameter = containerWidth * (((0.5 - 1) / (2560-375)) * (containerWidth - 375) + 1) + 'px'; // make portals appropriate base size for screen width: 1x screen width at 375px, 0.5x screen width at 2560px
         x = (containerWidth / 2) * pos.x + 'px';
-        y = (containerHeight / 2) * pos.y + 'px';
+        y = (containerHeight / 2) * pos.y + (.1 * scrollY) + 'px';
         z = 100 * pos.z + 'px';
+        i = face.x;
+        j = face.y;
+        k = face.z;
+        t = face.a;
     }
 
 </script>
@@ -22,25 +26,28 @@
         position: absolute;
         top: 50%;
         left: 50%;
-        border-radius: 540px;
 
         backface-visibility: hidden;
+        transition: box-shadow .3s;
+        &:hover {
+            box-shadow: 0 0 50px 10px white;
+        }
     }
 </style>
 
 <div class="portal" style="
     height: {diameter};
     width: {diameter};
+    border-radius: {diameter};
     background: {bg};
     transform:
         translate(-50%, -50%)
         translate3d({x}, {y}, {z})
         scale3d({scale}, {scale}, {scale})
-        rotateX({face.x})
-        rotateY({face.y});
+        rotate3d({i}, {j}, {k}, {t})
 ">
     <div style="margin: auto; text-align: center; padding-top: 20%; font-size: 32px;">
-        {diameter}<br>
-        {x}, {y}, {z}
+        <!-- {diameter}<br>
+        {x}, {y}, {z} -->
     </div>
 </div>
--- src/components/Portals.svelte
+++ src/components/Portals.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-    import Portal from '../components/Portal.svelte';
-    // export let y;
+    import Portal from './Portal.svelte'
+    export let scrollY;
 
     let containerWidth:number = 0,
 		containerHeight:number = 0;
 		
 	const portals = [
 		{
-			scale: .4,
-			pos: { x: -.8, y: -.8, z: 0 },
-			face: { x: '10deg', y: '-10deg', z: 0 },
+			scale: .5,
+			pos: { x: -.8, y: -.8, z: -3 },
+			face: { x: '-1', y: '1', z: '0', a: '70deg' },
 			bg: 'red'
 		},{
-			scale: .35,
-			pos: { x: .8, y: -.75, z: 0 },
-			face: { x: '10deg', y: '15deg', z: 0 },
+			scale: .45,
+			pos: { x: .8, y: -.75, z: -3 },
+			face: { x: '-1', y: '-1', z: '-.5', a: '60deg' },
 			bg: 'pink'
 		},{
-			scale: .2,
-			pos: { x: -.15, y: -.2, z: 0 },
-			face: { x: '-29deg', y: '-16deg', z: 2 },
+			scale: .25,
+			pos: { x: -.15, y: -.2, z: -3 },
+			face: { x: '.5', y: '1', z: '0', a: '40deg' },
 			bg: 'blue'
 		},{
-			scale: .35,
-			pos: { x: -.65, y: .65, z: 0 },
-			face: { x: '-15deg', y: '-15deg', z: 0 },
+			scale: .45,
+			pos: { x: -.65, y: .65, z: -3 },
+			face: { x: '1', y: '1', z: '-.3', a: '65deg' },
 			bg: 'green'
 		},{
-			scale: .6,
-			pos: { x: .55, y: .8, z: 0 },
-			face: { x: '-20deg', y: '12deg', z: 0 },
+			scale: .7,
+			pos: { x: .55, y: .8, z: -3 },
+			face: { x: '1', y: '-1', z: '.5', a: '60deg' },
 			bg: 'purple'
 		}
 	]
@@ -41,14 +41,15 @@
 	#container {
         position: relative;
         height: 50rem;
-        perspective: 500px;
+        perspective: 2000px;
 		overflow: visible;
     }
 </style>
 
 <div id="container" bind:clientWidth={containerWidth} bind:clientHeight={containerHeight}>
-	{containerWidth}, {containerHeight}
+	{containerWidth}, {containerHeight}<br>
+	{scrollY}
 	{#each portals as portal, i}
-		<Portal {containerWidth} {containerHeight} {...portal}/>
+		<Portal {containerWidth} {containerHeight} {scrollY} {...portal}/>
 	{/each}
 </div>
